d0d0a6546603a47d4832545a6378d79d1b03155c
Maarkhu <keramsli@wp.pl>
2023-01-22 14:07:25 +0100

    Parameters in the context of req and res, writing in file, async await.

--- W1/D4/index1.js
+++ W1/D4/index1.js
@@ -1,24 +1,28 @@
 const express = require('express');
+const {readFile, writeFile} = require('fs').promises;
+
 
 const app = express();
-let name;
 
-app.get('/name/set/:name', (req, res) => {
-    name = req.params.name;
+
+app.get('/name/set/:name', async (req, res) => {
+    const name = req.params.name;
+    await writeFile('./text.txt', name, "utf8");
     console.log(name);
     res.send(name);
     res.end();
 })
-app.get('/name/show/', (req, res) => {
+app.get('/name/show/', async (req, res) => {
+    const name = await readFile('text.txt', "utf-8");
     res.send(name);
     res.end();
 })
-app.get('/name/check', (req, res) => {
+app.get('/name/check', async (req, res) => {
 
-    if (name !== undefined) {
-        res.send(`Your name is written`);
-    } else {
-        res.send(`There is no name written`);
+    try { await readFile('text.txt');
+    res.send(`Your name is written.`);
+    } catch (e) {
+        res.send(`There is no name!`);
     }
         res.end();
 })
