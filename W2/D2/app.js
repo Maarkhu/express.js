f3cc95d85c6ab72fc2d4a5720286d0226e52f3e6
Maarkhu <keramsli@wp.pl>
2023-02-10 14:14:55 +0100

    File structure, voting.

--- W2/D2/app.js
+++ W2/D2/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const {nameRouter} = require("./routes/names");
+
+const limiter = rateLimit(
+    {
+        windowMs: 60 * 1000,
+        max: 10,
+    }
+);
+
 
 
 const app = express();
 
 app.use(express.json());
 app.use('/name', nameRouter)
-
+app.use(limiter);
 
 app.listen(3000, 'localhost');
