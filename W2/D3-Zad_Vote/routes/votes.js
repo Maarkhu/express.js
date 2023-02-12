f4432dd59ba9aa550ba41a9543c79b4bd7830741
Maarkhu <keramsli@wp.pl>
2023-02-12 16:51:49 +0100

    Voting, ip-restricter homework2.

--- W2/D3-Zad_Vote/routes/votes.js
+++ W2/D3-Zad_Vote/routes/votes.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const voteRouter = express.Router();
+const {IpRestricter} = require('../Utils/ip-restricter');
 
-let voteYes = 0;
-let voteNo = 0;
+const ipRestrict = new IpRestricter();
 
+// let voteYes = 0;
+// let voteNo = 0;
+const votes = {
+    yes: 0,
+    no: 0,
+}
 voteRouter
-
-    .get('/yes', async (req, res) => {
-        voteYes = Number(voteYes) + 1;
-        res.send(`Thanks for your vote!`);
-        res.end();
-    })
-    .get('/no', async (req, res) => {
-        voteNo = Number(voteNo) + 1;
-        res.send(`Thanks for your vote!`);
-        res.end();
-    })
     .get('/check', async (req, res) => {
-        res.send(`Vote for yes: ${voteYes} Vote for no: ${voteNo}`);
+        const info = Object.entries(votes).map(arr => `Votes on ${arr[0]}: ${arr[1]}`).join(`<br>`);
+        res.send(info);
+        //res.send(`Vote for yes: ${votes.yes} Vote for no: ${votes.no} <p>Maybe: ${votes.maybe}</p>`);
+    })
+    .get('/:voteName', async (req, res) => {
+        if (!ipRestrict.check(req.ip)) {
+            return res.status(403).send('Sorki oddałeś już głos');
+        }
+        const {voteName} = req.params;
+        if (typeof votes[voteName] === "undefined") {
+            votes[voteName] = 0;
+        };
+        votes[voteName]++;
+        res.send(`Thanks for your vote!`);
         res.end();
     })
 
